0a98acf370de0c210f42a62335bae0da2dfa265f
siyamkumar <siyamkumar@gmail.com>
2023-05-21 19:56:52 +0530

    v0.1.3

--- resources/views/homepage/gallery.blade.php
+++ resources/views/homepage/gallery.blade.php
@@ -17,7 +17,7 @@
 
 
         <div>
-            <x-hyperlinkbutton href="{{ route('publicnews') }}" class="" :outline="true">
+            <x-hyperlinkbutton href="{{ route('publicgallery') }}" class="" :outline="true">
                 All Gallery <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                     stroke="currentColor" width="20">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
