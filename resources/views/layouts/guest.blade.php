ac001a33652a9ebacd3c3e79d7cf9be0aa5aeda2
siyamkumar <siyamkumar@gmail.com>
2023-07-05 22:43:28 +0530

    v1.0.6

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="shortcut icon" href="#" />
     <title>{!! $title ? $title . ' | ' : '' !!} {{ config('app.name', 'AVNL') }}</title>
     {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
