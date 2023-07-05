f139335d5de1dd2e4607418afa5bae8e8f01dd64
siyamkumar <siyamkumar@gmail.com>
2023-07-05 23:01:50 +0530

    v1.1.0

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,68 +7,7 @@ import 'bootstrap';
 // window.Alpine = Alpine;
 
 // Alpine.start();
-
-import $ from 'jquery';
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
 import 'owl.carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 
-window.bootstrap  = bootstrap ;
-window.$ = window.jQuery = $;
-$(document).ready(function () {
-
-    // Carousel Initialization
-    $('#footerCarousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        nav: false,
-        // navText : ["",""],
-        slideTransition: 'linear',
-        autoplayTimeout: 3000,
-        autoplaySpeed: 3000,
-
-        autoWidth: true,
-        margin: 25,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 8
-            }
-        }
-    });
-
-    //Birthday Carousel
-
-    $('#birthdayCarousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        center:true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        
-        items:1,
-        autoplayHoverPause: true,
-    });
-    
-
-    $('#retirementCarousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        center:true,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        
-        items:1,
-        autoplayHoverPause: true,
-    });
-
-});
 
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,65 @@
 {{-- Logos --}}
 
+<script type="module">
+    
+$(document).ready(function () {
+
+// Carousel Initialization
+$('#footerCarousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    nav: false,
+    // navText : ["",""],
+    slideTransition: 'linear',
+    autoplayTimeout: 3000,
+    autoplaySpeed: 3000,
+
+    autoWidth: true,
+    margin: 25,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 8
+        }
+    }
+});
+
+//Birthday Carousel
+
+$('#birthdayCarousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    center:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    
+    items:1,
+    autoplayHoverPause: true,
+});
+
+
+$('#retirementCarousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    center:true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    
+    items:1,
+    autoplayHoverPause: true,
+});
+
+});
+</script>
+
 
 <div id="footerCarousel" class="owl-carousel">
     <div class="logo-slide">
